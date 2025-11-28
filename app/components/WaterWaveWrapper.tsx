a8a026674f4ac068ec10f6d4a632ed7540ecaeee
ImTalhaArif <62965957+ImTalhaArif@users.noreply.github.com>
2025-11-28 05:20:08 +0500

    Update WaterWaveWrapper.tsx

--- app/components/WaterWaveWrapper.tsx
+++ app/components/WaterWaveWrapper.tsx
@@ -1,1 +1,31 @@
+"use client"; // Ensure this runs only on client
 
+import dynamic from "next/dynamic";
+import { ReactNode } from "react";
+
+const WaterWaveDynamic = dynamic(() => import("react-water-wave"), { ssr: false });
+
+interface Methods {
+  update: () => void;
+}
+
+interface WaterWaveWrapperProps {
+  children: (methods: Methods) => ReactNode;
+  className?: string;
+  dropRadius?: number;
+  perturbance?: number;
+}
+
+const WaterWaveWrapper = ({ children, className, dropRadius, perturbance }: WaterWaveWrapperProps) => {
+  return (
+    <WaterWaveDynamic
+      className={className}
+      dropRadius={dropRadius}
+      perturbance={perturbance}
+    >
+      {(methods: Methods) => children(methods)}
+    </WaterWaveDynamic>
+  );
+};
+
+export default WaterWaveWrapper;
