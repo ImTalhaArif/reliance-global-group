b3ac156fb040dd82979dc643e93f34a17b624bb5
ImTalhaArif <62965957+ImTalhaArif@users.noreply.github.com>
2025-11-28 07:02:06 +0500

    Update WaterWaveBox.tsx

--- app/components/WaterWaveBox.tsx
+++ app/components/WaterWaveBox.tsx
@@ -1,1 +1,30 @@
+"use client";
 
+import dynamic from "next/dynamic";
+import { ReactNode } from "react";
+
+const WaterWave = dynamic(() => import("react-water-wave"), { ssr: false });
+
+interface WaterWaveBoxProps {
+  children: (methods: { update: () => void }) => ReactNode;
+  className?: string;
+  dropRadius?: number;
+  perturbance?: number;
+}
+
+export default function WaterWaveBox({
+  children,
+  className,
+  dropRadius,
+  perturbance,
+}: WaterWaveBoxProps) {
+  return (
+    <WaterWave
+      className={className}
+      dropRadius={dropRadius}
+      perturbance={perturbance}
+    >
+      {(methods: { update: () => void }) => children(methods)}
+    </WaterWave>
+  );
+}
