296c96e5917e89c92616cfc79ca46c84bb26e98e
ImTalhaArif <62965957+ImTalhaArif@users.noreply.github.com>
2025-11-28 05:27:39 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { motion, AnimatePresence } from "framer-motion";
 import { Dialog } from "@headlessui/react";
-import WaterWaveWrapper from "../components/WaterWaveWrapper";
+import WaterWaveWrapper from "./components/WaterWaveWrapper";
 
 interface LayoutProps {
   children: ReactNode;
